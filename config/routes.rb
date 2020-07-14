b754ca994f2e529c202d1234bf3486f91a028753
Scott <scottepick@gmail.com>
2020-07-13 23:07:00 -0400

    added Rails models and controllers for users and posts

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts
+  
+
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :create
+end
