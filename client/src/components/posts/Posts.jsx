56708d63e1c9fbfbf49eb3e9bd1caa89fee8143c
Scott <scottepick@gmail.com>
2020-07-19 14:13:15 -0400

    added css styling for post and posts

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import { withRouter, Link } from 'react-router-dom';
 const Posts = (props) => {
  const posts = props.data.posts;
+ // https://stackoverflow.com/questions/34860277/truncate-text-in-reactjs/36670484
+ const shortenText = (str) => {
+  return str.length > 500 ? str.substring(0, 550) + '...' : str;
+ }
  return (
   <div>
    {posts && posts.map((post, i) => (
-    <Link to={`/posts/${post.id}`}>
-     <div key={i} className='posts'>
-      <div className="posts__container">
-       <img className="posts--image" src={post.post_photo} alt={post.post_title} />
-      </div>
-      <div className="post__container">
-       <p className="posts--title">{post.post_title}</p>
-      </div>
-      <div className="post__container">
-       <p className="posts--content">{post.post_content}</p>
-      </div>
-
-
+    // <Link to={`/posts/${post.id}`}>
+    <div key={i} className='posts'>
+     <div className="posts__container">
+      <img className="posts--image" src={post.post_photo} alt={post.post_title} />
      </div>
-    </Link>
+     <div className="post__container">
+      <h2 className="posts--title">{post.post_title}</h2>
+     </div>
+     <div className="post__container">
+      <p className="posts--content">{shortenText(post.post_content)}</p>
+     </div>
+     <div className="posts--link">
+      <Link to={`/posts/${post.id}`}>read more</Link>
+     </div>
+
+    </div>
+    // </Link>
 
    ))}
   </div>
--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import './shared.scss';
 import Nav from './Nav';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
 const Header = (props) => {
  const { currentUser, handleLogout } = props
 
 
  return (
   <header>
-   <Link exact to="/">Header</Link>
-   <Link exact to="/posts/new">Create Post</Link>
+   <Link exact to="/">home</Link>
+   <Link exact to="/posts/new">create post</Link>
    <Nav />
    <button onClick={handleLogout}>logout</button>
   </header>
