4aff96124f2635dcadf77f5ccf865d5cde2e8155
Scott <scottepick@gmail.com>
2020-07-21 11:28:25 -0400

    fixed deploy bug

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://scotts-blog.herokuapp.com/' : 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://scotts-blog.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
  baseURL: baseURL
 })
 
 export default api;
+
