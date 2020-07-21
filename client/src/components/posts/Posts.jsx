bc6cc5ddf1ccfd38f5661fce0f0506c61020f0b0
Scott <scottepick@gmail.com>
2020-07-21 13:17:08 -0400

    bug fix number 2

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -11,28 +11,28 @@ const Posts = (props) => {
  return (
   <div className="postsContainer">
    <h2 className="post-h2">posts</h2>
-   {posts && posts.map((post, i) => (
-    <Link to={`/posts/${post.id}`}>
-     <div key={i} className='posts'>
-      <div className="posts__container">
-       <img className="posts--image" src={post.post_photo} alt={post.post_title} />
-      </div>
-      <div className="post__container">
-       <h2 className="posts--title">{post.post_title}</h2>
-      </div>
-      <div className="post__container">
-       <p className="posts--content">
-        <FontAwesomeIcon icon={faQuoteLeft} className="quotes left" />
-        {shortenText(post.post_content)}
-        <FontAwesomeIcon icon={faQuoteRight} className="quotes right" />
-       </p>
-      </div>
-      {/* <div className="posts--link">
-      <Link to={`/posts/${post.id}`}>read more!</Link>
-     </div> */}
-
+   {posts.map((post, i) => (
+    // <Link to={`/posts/${post.id}`}>
+    <div key={i} className='posts'>
+     <div className="posts__container">
+      <img className="posts--image" src={post.post_photo} alt={post.post_title} />
      </div>
-    </Link>
+     <div className="post__container">
+      <h2 className="posts--title">{post.post_title}</h2>
+     </div>
+     <div className="post__container">
+      <p className="posts--content">
+       <FontAwesomeIcon icon={faQuoteLeft} className="quotes left" />
+       {shortenText(post.post_content)}
+       <FontAwesomeIcon icon={faQuoteRight} className="quotes right" />
+      </p>
+     </div>
+     <div className="posts--link">
+      <Link to={`/posts/${post.id}`}>read more!</Link>
+     </div>
+
+    </div>
+    // </Link>
 
    ))}
   </div>
